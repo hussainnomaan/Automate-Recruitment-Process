ff7f33c6a5bf10ddd595d6ec6d9b6c772add5e4c
Muhammad Hamza <hamzaadil56@gmail.com>
2024-08-21 22:51:41 +0500

    added textarea

--- recruitment/frontend/automate-recruitment/src/App.tsx
+++ recruitment/frontend/automate-recruitment/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import './App.css'
+// import './App.css'
 import AgentCard from './components/agentCard'
 import ResearchLogo from './assets/research.png'
 import LinkLogo from './assets/link.png'
@@ -13,9 +13,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <main className='w-full h-screen flex flex-col justify-between text-blue-600'>
+    <main className='w-full max-h-screen h-screen flex flex-col justify-between text-blue-600 p-4'>
           <div>
-      <h1 className='text-3xl font-bold'>
+      <h1 className='text-3xl font-bold text-center'>
       HR Recruitment Automated Recruitment Process
       </h1>
           </div>
@@ -26,7 +26,25 @@ function App() {
           <AgentCard logo={ReporterLogo}/>
 
           </div>
-          <div></div>
+          <div className='w-full max-w-[700px]  mx-auto  relative'>
+          {/* <label
+            htmlFor="message"
+             className="block  text-sm font-medium "
+           >
+    Your message
+  </label> */}
+  <textarea
+    id="message"
+    rows={6}
+    
+    className="block p-2.5 w-full text-sm text-gray-900  rounded-lg border border-gray-300 focus:ring-blue-500 bg-[#d5e1f7] focus:border-blue-500    "
+    placeholder="Write your job description here..."
+    defaultValue={""}
+  />
+  <button className='absolute bottom-0 right-0 bg-blue-900 m-2 text-white rounded-full w-[40px] h-[40px]'>send</button>
+
+
+          </div>
     
     </main>
   )
